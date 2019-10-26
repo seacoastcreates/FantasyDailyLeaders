6c755d1ca3cb70a91ad455d1579b80c97e320498
Kirby Robinson <krobinson@mdconnectinc.com>
2019-10-26 18:34:58 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 import './App.css';
 
 
+
 function App() {
 
 	const [data, setData] = useState({ teams: [] });
@@ -12,28 +13,45 @@ function App() {
 			const result = await axios('https://fantasy.espn.com/apis/v3/games/fba/seasons/2020/segments/0/leagues/64997217?view=mDraftDetail&view=mLiveScoring&view=mMatchupScore&view=mPendingTransactions&view=mPositionalRatings&view=mSettings&view=mTeam&view=modular&view=mNav');
 
 			setData(result.data);
-
 			
-			// const myHeaders = new Headers();
-			// myHeaders.append(
-			// 	'Access-Control-Allow-Origin', '*'
-			// );
+			const myHeaders = new Headers();
+			myHeaders.append(
+				'Access-Control-Allow-Origin', '*'
+			);
 
 		}
 		fetchData();
-		
-	}, []);
-		
 
+		
+		
+		// const orderPoints( a, b ) {
+		// 	if ( a.team.points < b.team.points ){
+		// 		return -1;
+		// 	}
+		// 	if ( a.team.points > b.team.points ){
+		// 		return 1;
+		// 	}
+		// 	return 0;
+		// }
+		
+		
+	
+	}, []);
+
+		
+	
+	
   return (
+
     <div className="App container">
       {/* <header className="App-header">
        <h1>NBA FANTASY PLAYER OF THE NIGHT</h1>
       </header> */}
 			<main>
 				<div className="grid">
-
-					{data.sort()}
+				
+				
+				
 					{data.teams.map(team => (
 						<div key={team.id} className="grid-item">
 							<h2>{team.points}</h2>
