4f6e9443636766e3668722533473652c9f6ef00a
Kirby Robinson <krobinson@mdconnectinc.com>
2019-12-06 15:05:58 -0500

    sort points in descending order

--- src/App.js
+++ src/App.js
@@ -1,89 +1,84 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 import './App.css';
-
+// import { Fetch } from './Fetch'
 
 
 function App() {
 
-	const [data, setData] = useState({ teams: [] });
+	const [data, setData] = useState({ 
+		teams: [], 
+		schedule: [] 
+	});
 
 	useEffect(() => {
 		const fetchData = async () => {
-			const result = await axios('https://fantasy.espn.com/apis/v3/games/fba/seasons/2020/segments/0/leagues/64997217?view=mDraftDetail&view=mLiveScoring&view=mMatchupScore&view=mPendingTransactions&view=mPositionalRatings&view=mSettings&view=mTeam&view=modular&view=mNav');
+			const result = await axios('https://fantasy.espn.com/apis/v3/games/fba/seasons/2020/segments/0/leagues/64997217?view=modular&view=mNav&view=mMatchupScore&view=mRoster&view=mScoreboard&view=mSettings&view=mTopPerformers&view=mTeam');
 
 			setData(result.data);
-			
+
 			const myHeaders = new Headers();
 			myHeaders.append(
 				'Access-Control-Allow-Origin', '*'
 			);
 
-			
 		}
 		fetchData();
 		
 	}, []);
+
 	
 	let teams = data.teams;
-	teams.forEach(teams => {
-		let points = teams.points;
-		
-		
-		
-		// points.sort(function(a, b){return b - a}); 
-		
 
-		console.log(points);
-	
+	teams.forEach( (team) => {
+		let seasonPointTotal = teams.points;	
 
-		// let sortedPoints = points.sort(function(a,b) {
-		// 	return a.points - b.points;
-		// });
-		// console.log(sortedPoints);
-		
-
-		
+		teams.sort((a, b) => (a.points < b.points) ? 1 : -1);		
 	});
 
 
+	let schedule = data.schedule;
+
+	schedule.forEach( (matchup) => {
+		for (let fantasyTeam in matchup) {
+			let fantasyStats = (matchup[fantasyTeam]);
+			if (fantasyStats.hasOwnProperty('rosterForMatchupPeriodDelayed')){
+				// console.log(fantasyStats);
+				let fantasyPoints = fantasyStats.totalPointsLive;
+				let fantasyTeamID = fantasyStats.teamId;
+				// return fantasyStats;
+				console.log(fantasyPoints, fantasyTeam, fantasyTeamID);
+			} 
+			
+
+		}
+	})
+
+	function makeIt(fantasyStats) {
+
+	}
+
+
+	// function getMatchup(matchups) {
+
+	// }
 
   return (
 
     <div className="App container">
-      {/* <header className="App-header">
-       <h1>NBA FANTASY PLAYER OF THE NIGHT</h1>
-      </header> */}
+			<header>
+				<h1>Weekly Fantasy Leaders</h1>
+			</header> 
 			<main>
 				<div className="grid">
-				
-			
-		
-				
-				
 					{data.teams.map(team => (
 						<div key={team.id} className="grid-item">
-						
-							<h2>{team.points}</h2>
+							<h2></h2>
 							<img src={team.logo} alt="" />
 							<h3>{team.location} {team.nickname}</h3>
+							<h4>Season Total: {team.points}</h4>
 						</div>
-
-			
 					))}	
-
-				
-
-
-
-					{/* {data.teams.map(team => (
-						<div key={team.id} className="grid-item">
-							<h2>{team.points}</h2>
-							<img src={team.logo} />
-							<h3>{team.location} {team.nickname}</h3>
-						</div>
-					))} */}
-
 				</div>
 			</main>
 			<footer></footer>
--- src/Fetch.js
+++ src/Fetch.js
@@ -4,14 +4,17 @@ import axios from 'axios'
 
 function Fetch() 	{
 	
-	const [data, setData] = useState({ teams: [] });
+	const [data, setData] = useState({ 
+		teams: [], 
+		schedule: [] 
+	});
 
 	useEffect(() => {
 		const fetchData = async () => {
-			const result = await axios('https://fantasy.espn.com/apis/v3/games/fba/seasons/2020/segments/0/leagues/64997217?view=mDraftDetail&view=mLiveScoring&view=mMatchupScore&view=mPendingTransactions&view=mPositionalRatings&view=mSettings&view=mTeam&view=modular&view=mNav');
+			const result = await axios('https://fantasy.espn.com/apis/v3/games/fba/seasons/2020/segments/0/leagues/64997217?view=modular&view=mNav&view=mMatchupScore&view=mRoster&view=mScoreboard&view=mSettings&view=mTopPerformers&view=mTeam');
 
 			setData(result.data);
-			
+
 			const myHeaders = new Headers();
 			myHeaders.append(
 				'Access-Control-Allow-Origin', '*'
@@ -19,7 +22,7 @@ function Fetch() 	{
 
 		}
 		fetchData();
-	
+		
 	}, []);
 
 }
